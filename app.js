224470244d94e0bf769d907daa1ee2386973c5ee
Kimoilmagnifico <smattia.sozzi@itis.pr.it>
2026-04-21 09:25:08 +0200

    Add registration and login routes to app.js

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const port = 3000;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var registratiRouter = require('./routes/registrati');
+var loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -25,6 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/registrati', registratiRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
