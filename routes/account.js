908765e8f0f36e72c6aab584b44e5ace160de3d7
Kimoilmagnifico <smattia.sozzi@itis.pr.it>
2026-05-11 21:07:48 +0200

    Restile schemrata home

--- routes/account.js
+++ routes/account.js
@@ -15,6 +15,40 @@ function requireLogin(req, res, next) {
     next();
 }
 
+
+const memoriaAccessi = {};
+const rateLimiterManuale = (req, res, next) => {
+    const ip = req.ip; 
+    const ORA_ATTUALE = Date.now();
+    const LIMITE_TEMPO = 60000; 
+    const MAX_RICHIESTE = 10;   
+
+    console.log(`IP ${ip} ha fatto una richiesta. Conteggio attuale: ${memoriaAccessi[ip]?.conteggio || 0}`);
+
+    if (!memoriaAccessi[ip]) {
+        
+        memoriaAccessi[ip] = { conteggio: 1, inizioFinestra: ORA_ATTUALE }; //Primo accesso dell'IP
+    } else {
+
+        if (ORA_ATTUALE - memoriaAccessi[ip].inizioFinestra > LIMITE_TEMPO) {           //Se è pssato più di un minuto, resetta il conteggio
+            memoriaAccessi[ip].conteggio = 1;
+            memoriaAccessi[ip].inizioFinestra = ORA_ATTUALE;
+        } else {
+            memoriaAccessi[ip].conteggio++;
+        }
+    }
+
+    if (memoriaAccessi[ip].conteggio > MAX_RICHIESTE) {
+        return res.render('account', {
+            title: 'Account',
+            user: req.session.user,
+            apiKey: 'Troppe richieste. Riprova tra un minuto.'
+        });
+    }
+
+    next();
+};
+
 router.get('/', requireLogin, function(req, res) {
 
     const user = {
@@ -71,9 +105,14 @@ router.post('/delete', requireLogin, (req, res) => {
 });
 
 
-router.post('/request-api-key', requireLogin, (req, res) => {
+
+router.post('/request-api-key', rateLimiterManuale, requireLogin, (req, res) => {
     
+    //TODO: aggiungere un controllo per evitare di generare una nuova chiave se l'utente ne ha già una valida a meno che non voglia esplicitamente rigenerarla
+    
+
     try {
+
         const newApiKey = crypto.randomBytes(32).toString('hex');      //Genera una stringa casuale di 32 byte in formato esadecimale
         
         const stmt1 = db.prepare(sqlApiKey);
