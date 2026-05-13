7f9e2e1e592f90acd3872777819164bbe5cd15f7
Kimoilmagnifico <smattia.sozzi@itis.pr.it>
2026-05-13 11:48:20 +0200

    Correzione css e eliminazione pagina obbiettivo

--- routes/obiettivo.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res, next) {
-  res.render('obiettivo', { title: 'Il nostro obiettivo', user: req.session?.user || null });
-});
-
-module.exports = router;
